f71511c72591f59173e8a72f6d858d3255bc6d25
irvankadhafi <irvan.kadhafi.tif18@polban.ac.id>
2020-02-13 16:51:09 +0700

    first commit

---
+++ app/src/main/java/com/irvankdhf/kdhf/Dialog.java
@@ -0,0 +1,26 @@
+package com.irvankdhf.kdhf;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class Dialog extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public android.app.Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Informasi Kontak")
+                .setMessage("Email saya irvan.kadhafi.tif18@polban.ac.id dan nomor hp saya +6281927145985")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
